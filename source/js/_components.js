807b83df71fd3d3e5def24f79f6fdeacf53bcfaa
roman <romasaab@gmail.com>
2024-11-05 19:02:13 +0200

    first commit

---
+++ source/js/_components.js
@@ -0,0 +1,10 @@
+import './components/dinamicHeight.js';
+import './components/sliders.js';
+import './components/modals.js';
+import './components/acc.js';
+import './components/fancybox.js';
+import './components/mobile-menu.js';
+import './components/tabs.js';
+import './components/anchor.js';
+import './components/toTop.js';
+
---
+++ source/js/_vars.js
@@ -0,0 +1,26 @@
+export default {
+  windowEl: window,
+  documentEl: document,
+  htmlEl: document.documentElement,
+  bodyEl: document.body,
+  activeClass: 'active',
+  activeClassMode: 'mode',
+  header: document.querySelector('header'),
+  footer: document.querySelector('footer'),
+
+  mainSliders: document.querySelectorAll('.review-slider'),
+
+  
+  infoSliders: document.querySelectorAll('.info-slider'),
+  cookingSliders: document.querySelectorAll('.cooking-slider'),
+  parentSliders: document.querySelectorAll('.product-slider'),
+  filterBtn: document.querySelector('[data-aside-btn]'),
+  filterAside: document.querySelector('.product-section__aside'),
+
+  burger: document.querySelectorAll('.burger'),
+  mobileMenu: document.querySelector('.mobile'),
+  overlay: document.querySelector('[data-overlay]'),
+  modals: [...document.querySelectorAll('[data-popup]')],
+  modalsMode: [...document.querySelectorAll('[data-mode-modal]')],
+  modalsButton: [...document.querySelectorAll("[data-btn-modal]")],
+}
