16d46658980f48162fa0665d62dc917906f55d88
roman <romasaab@gmail.com>
2024-11-05 20:02:06 +0200

    add section

--- source/js/components/tabs.js
+++ source/js/components/tabs.js
@@ -44,7 +44,6 @@ const tabsFunction = function (
 
 document.addEventListener("DOMContentLoaded", function () {
   tabsFunction(document.querySelectorAll("[data-tabs-parrent]"), "data-tab", "data-tab-content");
-  tabsFunction(document.querySelectorAll("[data-inner-parrent]"), "data-inner-tab", "data-inner-content");
 });
 
 
